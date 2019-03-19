43c4b36d3bc8dc861dabad930bddaa50f3f0e807
Arbeker01 <romeogono1@aol.com>
2019-03-18 19:58:23 -0700

    add daily_activities controller and corresponding CRUD routes

---
+++ app/controllers/daily_activities_controller.rb
@@ -0,0 +1,37 @@
+class DailyActivitesController < ApplicationController
+
+get '/daily_activities'  do
+	@daily_activities = DailyActivity.all 
+	erb :'/all_daily_activities'
+end
+
+get '/daily_activities/new'  do
+	erb :'/daily_activities/new'
+end
+
+get '/daily_activities/:id' do
+	@daily_activity  = DailyActivity.find_by_id(params[:id])
+	erb :'/daily_activities/show'
+end
+
+post '/daily_activities' do
+	@daily_activity = DailyActivity.create(location: params[:location], description: params[:description], books: params[:books], medication: params[:medication], care_giver_id: current_user.id)
+	redirect '/daily_activities/#{@daily_activity.id}'
+end
+
+get '/daily_activities/:id/edit' do
+	erb :'/daily_activities/edit'
+end
+
+patch '/daily_activities/:id' do
+	@daily_activity = DailyActivity.find_by_id(params[:id])
+	@daily_activity.update
+	redirect '/daily_activities/#{@daily_activity.id}'
+end
+
+delete '/daily_activities/:id' do
+	@daily_activity = DailyActivity.find_by_id(params[:id])
+	@daily_activity.destroy	
+end
+
+end
