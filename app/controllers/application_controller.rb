e5185d97b4c8a38c6a70ce73fc8112834e749ec7
Arbeker01 <romeogono1@aol.com>
2019-04-10 17:54:57 -0700

    Delete intended time-of-day method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,16 +32,4 @@ class ApplicationController < Sinatra::Base
     @daily_activity = DailyActivity.find_by(id: params[:id])
   end
 
-  def greet
-  if Time.now.between? Time.now.beginning_of_day, Time.now.middle_of_day
-        puts "Good Morning"
-  elsif Time.now.between? Time.now.middle_of_day, Time.now.change(hour: 17)
-        puts "Good Afternoon"
-  elsif Time.now.between? Time.now.change(hour: 17), Time.now.change(hour: 20)
-        puts"Good Evening"
-  elsif Time.now.between? Time.now.change(hour: 20), Time.now.end_of_day
-         puts "Good Night"
-  end
-end
-
 end
